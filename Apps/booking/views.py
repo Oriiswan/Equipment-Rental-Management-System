d462e7e78f7c534a19bef7bc1f6fd4d8dfd85383
Michael <you@example.com>
2025-06-04 01:15:34 +0800

    delete function and updating equipment quantity

--- Apps/booking/views.py
+++ Apps/booking/views.py
@@ -29,14 +29,18 @@ def edit_booking(request, rental_id):
         
         rental_date_str = request.POST.get('rental_date')
         due_date_str = request.POST.get('due_date')
+        rental_date = datetime.strptime(rental_date_str, "%Y-%m-%d").date()
         customer_id = request.POST.get('customer')
         equipment_id = request.POST.get('equipment')
-        
+        equipment = Equipments.objects.get(pk=equipment_id)
         booking.customer = Customers.objects.get(customer_id=customer_id)
         booking.equipment = Equipments.objects.get(equipment_id=equipment_id)
         booking.rental_date = datetime.strptime(rental_date_str, "%Y-%m-%d").date()
         booking.due_date = datetime.strptime(due_date_str, "%Y-%m-%d").date()
         booking.save()
+        if rental_date <= date.today() and equipment.available_quantity > 0:
+            equipment.available_quantity -= 1
+            equipment.save()
         return redirect('booking_list')
     else:
         return render(request, 'apps/booking/edit_booking.html', {
@@ -89,22 +93,82 @@ def overdue_list(request):
       'total': total,
     })
 
+def delete_booking(request,rental_id):
+    rentals = rental.objects.all()
+    rental_obj = get_object_or_404(rental, pk=rental_id)
+    if request.method == 'POST':
+        record = rental.objects.filter(rental_id=rental_id)
+        record.delete()
+        return redirect('booking_list')
+    context = {
+        'booking': rental_obj,
+        'rentals': rental.objects.all(),
+        'overdue_count': rental.objects.filter(status='Overdue').count(),
+        'returned_count': rental.objects.filter(status='Returned').count(),
+        'active_count': rental.objects.filter(status='Active').count(),
+        'total':sum(item.total_amount for item in rentals),
+        'equipments': Equipments.objects.all(),
+        'customers': Customers.objects.all(),
+    }
+    return render(request, 'apps/booking/delete_booking.html', context)
+
+def delete_pending_list(request,rental_id):
+    rentals = rental.objects.all()
+    rental_obj = get_object_or_404(rental, pk=rental_id)
+    if request.method == 'POST':
+        record = rental.objects.filter(rental_id=rental_id)
+        record.delete()
+        return redirect('pending_list')
+    context = {
+        'booking': rental_obj,
+        'rentals': rental.objects.all(),
+        'overdue_count': rental.objects.filter(status='Overdue').count(),
+        'returned_count': rental.objects.filter(status='Returned').count(),
+        'active_count': rental.objects.filter(status='Active').count(),
+        'total':sum(item.total_amount for item in rentals),
+        'equipments': Equipments.objects.all(),
+        'customers': Customers.objects.all(),
+    }
+    return render(request, 'apps/booking/delete_pending.html', context)
+def delete_returned_list(request,rental_id):
+    rentals = rental.objects.all()
+    rental_obj = get_object_or_404(rental, pk=rental_id)
+    if request.method == 'POST':
+        record = rental.objects.filter(rental_id=rental_id)
+        record.delete()
+        return redirect('return_list')
+    context = {
+        'booking': rental_obj,
+        'rentals': rental.objects.all(),
+        'overdue_count': rental.objects.filter(status='Overdue').count(),
+        'returned_count': rental.objects.filter(status='Returned').count(),
+        'active_count': rental.objects.filter(status='Active').count(),
+        'total':sum(item.total_amount for item in rentals),
+        'equipments': Equipments.objects.all(),
+        'customers': Customers.objects.all(),
+    }
+    return render(request, 'apps/booking/delete_returned.html', context)
 def add_booking(request):
     if request.method == 'POST':
         try:
             # Get form data
+            now = date.today()
             customer_id = request.POST.get('customer')  # Required field
             equipment_id = request.POST.get('equipment')  # Required field
+            equipment = Equipments.objects.get(pk=equipment_id)
             rental_date = datetime.strptime(request.POST['rental_date'], '%Y-%m-%d').date()
             due_date = datetime.strptime(request.POST['due_date'], '%Y-%m-%d').date()
-            
-            # Validate dates
             if rental_date > due_date:
                 messages.error(request, "Due date must be after rental date")
                 return redirect('add_booking')
-            
+            elif equipment.available_quantity == 0:
+                messages.error(request, "Equipment is out of stock")
+                return redirect('add_booking')
+            # Validate dates
+           
             # Create rental
-            rental.objects.create(
+            else:
+                rental.objects.create(
                 customer_id=customer_id,
                 equipment_id=equipment_id,
                 rental_date=rental_date,
@@ -113,22 +177,26 @@ def add_booking(request):
                 rental_agreement=request.FILES.get('rental_agreement'),
                 status=determine_status(rental_date, due_date)
             )
-            records = rental.objects.all()
-            total = sum(item.total_amount for item in records)
-            # Update customer
-            
-            
-            messages.success(request, "Booking created successfully!")
-            return render(request, 'apps/booking/list.html', {
-              'rentals': records,
-              'equipments': Equipments.objects.all(),
-              'customers': Customers.objects.all(),
-              'overdue_count': rental.objects.filter(status='Overdue').count(),
-              'returned_count': rental.objects.filter(status='Returned').count(),
-              'active_count': rental.objects.filter(status='Active').count(),
-              'total': total
-              
-            })
+                
+                if now > rental_date:
+                    equipment.available_quantity -= 1
+                    equipment.save()
+                records = rental.objects.all()
+                total = sum(item.total_amount for item in records)
+                # Update customer
+                
+                
+                messages.success(request, "Booking created successfully!")
+                return render(request, 'apps/booking/list.html', {
+                'rentals': records,
+                'equipments': Equipments.objects.all(),
+                'customers': Customers.objects.all(),
+                'overdue_count': rental.objects.filter(status='Overdue').count(),
+                'returned_count': rental.objects.filter(status='Returned').count(),
+                'active_count': rental.objects.filter(status='Active').count(),
+                'total': total
+                
+                })
             
         except KeyError as e:
             messages.error(request, f"Missing required field: {str(e)}")
@@ -153,10 +221,12 @@ def determine_status(rental_date, due_date):
 
 def mark_as_returned(request, rental_id):
     # Get the specific rental object
+    rentals = rental.objects.all()
     rental_obj = get_object_or_404(rental, pk=rental_id)
     now = datetime.now()
     if request.method == 'POST':
         # Update status to Returned
+        
         rental_obj.status = 'Returned'
         rental_obj.return_date = timezone.now().date()  # Add if you track return dates
         rental_obj.updated_at = rental_obj.return_date
@@ -178,7 +248,7 @@ def mark_as_returned(request, rental_id):
         'overdue_count': rental.objects.filter(status='Overdue').count(),
         'returned_count': rental.objects.filter(status='Returned').count(),
         'active_count': rental.objects.filter(status='Active').count(),
-        'total': rental.objects.aggregate(total=Sum('calculated_amount'))['total'] or 0,
+        'total':sum(item.total_amount for item in rentals),
         'equipments': Equipments.objects.all(),
         'customers': Customers.objects.all(),
     }
